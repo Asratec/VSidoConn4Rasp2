1d4f770b12bfd1caeb0c6bfe5eb59202746a0ee4
Ma Eikei <yma@asratec.co.jp>
2015-03-16 18:44:13 +0900

    add raspberry pi 2 build script

--- Setup/setenv.sh
+++ Setup/setenv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 apt-get update 
-apt-get install git cmake --fix-missing
 apt-get install gcc-4.8 g++-4.8 --fix-missing
+apt-get install ngnix git cmake --fix-missing
 apt-get install libv4l-dev libopencv-dev --fix-missing
 cd /usr/bin && ln -sf gcc-4.8 gcc
 cd /usr/bin && ln -sf g++-4.8 g++
