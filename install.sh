0eac15bfdcaa97fe15cc02ab000155a9dc0bd9ba
Ma Eikei <yma@asratec.co.jp>
2015-03-30 13:52:40 +0900

    add ifttt sample & add dual camera & nodejs 0.10

--- install.sh
+++ install.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
+
+apt-get update 
+apt-get install gcc-4.8 g++-4.8 --fix-missing
+apt-get install ngnix git cmake --fix-missing
+apt-get install libv4l-dev libopencv-dev --fix-missing
+cd /usr/bin && ln -sf gcc-4.8 gcc
+cd /usr/bin && ln -sf g++-4.8 g++
+sync
+sync
+
 wget --no-check-certificate https://asratec.github.io/VSidoConn4Rasp2/Binary/v0.81/VSidoConn4Rasp2.tar.gz -O VSidoConn4Rasp2.tar.gz 
 sudo mkdir -p /opt/vsido/
 sudo tar -xzvf VSidoConn4Rasp2.tar.gz -C /opt/vsido/
 sudo make -C /opt/vsido/usr/share/Config
+
+wget --no-check-certificate https://asratec.github.io/VSidoConn4Rasp2/Binary/node-v0.10.28-linux-arm-pi.tar.gz -O node-v0.10.28-linux-arm-pi.tar.gz 
+sudo mkdir -p /opt/vsido/
+sudo tar -xzvf node-v0.10.28-linux-arm-pi.tar.gz -C /opt/vsido/
+sudo cp -rf /opt/vsido/node-v0.10.28-linux-arm-pi/* /usr/
+
+
 sync
 sync
 
