ee4b384aa681f4229611cff0a0ea348c3c74d60c
Ma Eikei <yma@asratec.co.jp>
2015-03-31 16:32:58 +0900

    change install scripts

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 apt-get update 
-apt-get install gcc-4.8 g++-4.8 --fix-missing
-apt-get install ngnix git cmake --fix-missing
-apt-get install libv4l-dev libopencv-dev --fix-missing
+apt-get install -y gcc-4.8 g++-4.8 --fix-missing
+apt-get install -y nginx git cmake --fix-missing
+apt-get install -y libv4l-dev libopencv-dev --fix-missing
 cd /usr/bin && ln -sf gcc-4.8 gcc
 cd /usr/bin && ln -sf g++-4.8 g++
 sync
