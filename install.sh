1696ea7e75695edab16639037f0d7d5d5d0f3164
Ma Eikei <yma@asratec.co.jp>
2015-11-06 20:17:42 +0900

    fix directory structure of binay v0.96

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ cd /usr/bin && ln -sf g++-4.8 g++
 sync
 sync
 
-wget --no-check-certificate https://asratec.github.io/VSidoConn4Rasp2/Binary/v0.82/VSidoConn4Rasp2.tar.gz -O VSidoConn4Rasp2.tar.gz 
+wget --no-check-certificate https://asratec.github.io/VSidoConn4Rasp2/Binary/v0.96/VSidoConn4Rasp2.tar.gz -O VSidoConn4Rasp2.tar.gz 
 sudo mkdir -p /opt/vsido/
 sudo tar -xzvf VSidoConn4Rasp2.tar.gz -C /opt/vsido/
 sudo make -C /opt/vsido/usr/share/Config
