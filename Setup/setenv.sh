7d85ab6ad2e426a909d71c684d00ff1e30110af1
Ma Eikei <yma@asratec.co.jp>
2015-11-22 02:58:58 +0000

    update to rasbian 8

--- Setup/setenv.sh
+++ Setup/setenv.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 apt-get update 
-apt-get install -y gcc-4.8 g++-4.8 --fix-missing
-apt-get install -y nginx git cmake --fix-missing
-apt-get install -y libv4l-dev libopencv-dev --fix-missing
-apt-get install -y rfkill bluez bluez-hcidump --fix-missing
+version=`cat /etc/debian_version | cut -b1-1`
+if [ $version -lt 8 ] ;then
+	apt-get install -y gcc-4.8 g++-4.8 --fix-missing
+	cd /usr/bin && ln -sf gcc-4.8 gcc
+	cd /usr/bin && ln -sf g++-4.8 g++
+fi
+apt-get install -y nginx git cmake libv4l-dev libopencv-dev rfkill bluez bluez-hcidump --fix-missing
 
-cd /usr/bin && ln -sf gcc-4.8 gcc
-cd /usr/bin && ln -sf g++-4.8 g++
 sync
 sync
-
-
-
 
