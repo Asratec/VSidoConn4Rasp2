9505a4563de1b34fb9f08962e8946f65af3b4d22
Ma Eikei <yma@asratec.co.jp>
2016-01-20 16:51:05 +0900

    update to rasbian 8

--- install.sh
+++ install.sh
@@ -1,29 +1,15 @@
 #!/bin/bash
-
-apt-get update 
-apt-get install -y gcc-4.8 g++-4.8 --fix-missing
-apt-get install -y nginx git cmake --fix-missing
-apt-get install -y libv4l-dev libopencv-dev --fix-missing
-apt-get install -y rfkill bluez bluez-hcidump --fix-missing
-
-cd /usr/bin && ln -sf gcc-4.8 gcc
-cd /usr/bin && ln -sf g++-4.8 g++
-sync
-sync
-
+sudo apt-get update 
+version=`cat /etc/debian_version | cut -b1-1`
+if [ $version -lt 8 ] ;then
+	apt-get install -y gcc-4.8 g++-4.8 --fix-missing
+	cd /usr/bin && ln -sf gcc-4.8 gcc
+	cd /usr/bin && ln -sf g++-4.8 g++
+fi
+sudo apt-get install -y --fix-missing nginx git cmake rfkill bluez bluez-hcidump libv4l-dev libopencv-dev nodejs
 wget --no-check-certificate https://asratec.github.io/VSidoConn4Rasp2/Binary/v0.96/VSidoConn4Rasp2.tar.gz -O VSidoConn4Rasp2.tar.gz 
 sudo mkdir -p /opt/vsido/
 sudo tar -xzvf VSidoConn4Rasp2.tar.gz -C /opt/vsido/
 sudo make -C /opt/vsido/usr/share/Config
-
-wget --no-check-certificate https://asratec.github.io/VSidoConn4Rasp2/Binary/node-v0.10.28-linux-arm-pi.tar.gz -O node-v0.10.28-linux-arm-pi.tar.gz 
-sudo mkdir -p /opt/vsido/
-sudo tar -xzvf node-v0.10.28-linux-arm-pi.tar.gz -C /opt/vsido/
-sudo cp -rf /opt/vsido/node-v0.10.28-linux-arm-pi/* /usr/
-
-
 sync
 sync
-
-
-
